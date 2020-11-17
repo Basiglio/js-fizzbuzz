52ddb4a5728b244e5a999cd4551f3cf09db6d533
Emilo <emilio.furnari87@gmail.com>
2020-11-17 14:44:46 +0100

    secondo push/completato

---
+++ js/myscript.js
@@ -0,0 +1,23 @@
+// FizzBuzz
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+// CREO UN CICLO FOR
+for (var i = 1; i <= 100; i++) {
+  // FACCIO STAMPARE I NUMERI DA 1 A 100
+  if(i % 3 == 0 && i % 5 == 0) {
+    // SE I(IL NUMERO) è MULTIPLO DI 3 ED ANCHE MULTIPLO DI 5 STAMPA FIZZBUZZ
+    document.getElementById('elenco_numeri').innerHTML += "<li>" + "FizzBuzz" + "</li>";
+  } else if (i % 3 == 0) {
+    // ALTRIMENTI SE IL NUMERO è SOLO MULTIPLO DI 3 STAMPA BUZZ
+    document.getElementById('elenco_numeri').innerHTML += "<li>" + "Buzz" + "</li>";
+  } else if (i % 5 == 0) {
+    // ALTRIMENTI SE IL NUMERO è SOLO MULTIPLO DI 5 STAMPA FIZZ
+    document.getElementById('elenco_numeri').innerHTML += "<li>" + "Fizz" + "</li>";
+  } else {
+    // ALTRIMENTI STAMPA IL NUMERO
+    document.getElementById('elenco_numeri').innerHTML += "<li>" + (i) + "</li>";
+  }
+}
